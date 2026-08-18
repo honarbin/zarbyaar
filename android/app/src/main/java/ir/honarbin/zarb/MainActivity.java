8e7df39ae93a8c0617eb7201b0c53f1eeebe8756
honarbin <honarbin@gmail.com>
2026-08-19 00:45:26 +0330

    build: setup MainActivity for Capacitor
    Initialize the MainActivity class in the Android project to support the Capacitor bridge.

---
+++ android/app/src/main/java/ir/honarbin/zarb/MainActivity.java
@@ -0,0 +1,11 @@
+package ir.honarbin.zarb;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
